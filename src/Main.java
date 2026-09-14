0700a3a9dfff6cf926f66b34dc8a41326967f637
Knyazeva Diana <knyazevaofficial@gmail.com>
2026-09-14 21:24:49 +0700

    первое задание

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,38 @@
-import java.util.Scanner;
 public class Main {
+    public static void main(String[] args){
+        System.out.println("1 zadanie" );
+        WholeNumber number = new WholeNumber();
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("1)" );
+        number.MinMax();
 
-        System.out.print("Enter the command: ");
+        System.out.println("2)" );
+        number.Summ();
 
-        String command = scanner.next();
+        System.out.println("3)" );
+        number.LongInt();
 
-        switch (command) {
-            case "fizzbuzz" -> FizzBuzz.fizzbuzz();
-            case "reverse" -> {
-                System.out.print("Enter a word: ");
-                String input = scanner.next();
-                String result = TextTasks.reverse(input);
-                System.out.println(result);
-            }
-            case "quadratic" -> {
-                System.out.println("Enter 3 real coefficients: ");
-                double a = scanner.nextDouble();
-                double b = scanner.nextDouble();
-                double c = scanner.nextDouble();
-                QuadraticEquation.quadratic(a, b, c);
-            }
+        System.out.println("4)" );
+        number.Division();
 
-            case "series" -> SeriesCalculator.series();
+        System.out.println("5)" );
+        number.LongToInt();
 
-            case "palindrome" -> {
-                scanner.nextLine();
-                System.out.print("Enter palindrome: ");
-                String input = scanner.nextLine();
-                Palindromes.palindrome(input);
-            }
+        System.out.println("6)" );
+        number.CharArithmetic();
 
-            default -> System.out.print("fizzbuzz\n" +
-                    "reverse <string>\n" +
-                    "quadratic <a> <b> <c>\n" +
-                    "series\n" +
-                    "palindrome <string>");
-        }
-        scanner.close();
+        System.out.println("7)" );
+        int a = Integer.MAX_VALUE;
+        int b = 676767;
+        number.SumInt(a, b);
 
+        int c = 200;
+        int d = 357;
+        number.SumInt(c, d);
+
+        int e = Integer.MIN_VALUE;
+        int f = -9999;
+        number.SumInt(e, f);
     }
+    
 }
